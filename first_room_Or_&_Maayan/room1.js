fec3d0a09ee44279ccf22d02a4f4e98f4d5bb849
TheGhoustking <orarzuan555@gmail.com>
2025-04-24 20:15:25 +0300

    canciling chat gpt nonsence

--- first_room_Or_&_Maayan/room1.js
+++ first_room_Or_&_Maayan/room1.js
@@ -1,3 +1,24 @@
 function moveOn(){
     window.location.href = "../room1.html";
 }
+
+let keysCollected = 0;
+
+    function handleWindow(num) {
+        if (num === 1) {
+            alert("נסיך עצוב מופיע… אולי כדאי לחפש מפתח?");
+        } else if (num === 2) {
+            if (keysCollected === 0) {
+                alert("מצאת מפתח! 🔑");
+                keysCollected = 1;
+            } else {
+                alert("כבר לקחת את המפתח 😊");
+            }
+        } else if (num === 3) {
+            if (keysCollected === 1) {
+                alert("מצאת את הורד! 🌹 סיימת את המשימה!");
+            } else {
+                alert("הדלת נעולה… אולי כדאי למצוא מפתח קודם?");
+            }
+        }
+    }
