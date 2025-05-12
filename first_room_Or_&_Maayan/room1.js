508eb2079cf7c062aa6a795444a67a2bc84d1e8c
TheGhoustking <orarzuan555@gmail.com>
2025-05-12 19:44:50 +0300

    popups

--- first_room_Or_&_Maayan/room1.js
+++ first_room_Or_&_Maayan/room1.js
@@ -3,11 +3,25 @@ function moveOn(){
 }
 
 function showPopup0() {
-    alert("לחצת על חלון 1!");
+    document.getElementById("popup").style.display = "block";
 }
+
+function hidePopup() {
+    document.getElementById("popup").style.display = "none";
+}
+
 function showPopup1() {
-    alert("לחצת על חלון 2!");
+    document.getElementById("popup1").style.display = "block";
 }
+
+function hidePopup1() {
+    document.getElementById("popup1").style.display = "none";
+}
+
 function showPopup2() {
-    alert("לחצת על חלון 3!");
+    document.getElementById("popup2").style.display = "block";
+}
+
+function hidePopup2() {
+    document.getElementById("popup2").style.display = "none";
 }
