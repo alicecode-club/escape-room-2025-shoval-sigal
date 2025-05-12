178b8afb951d3c64d6a7a34d1fd30670b27e48db
TheGhoustking <orarzuan555@gmail.com>
2025-05-12 19:19:36 +0300

    making press div

--- first_room_Or_&_Maayan/room1.js
+++ first_room_Or_&_Maayan/room1.js
@@ -1,3 +1,13 @@
 function moveOn(){
     window.location.href = "../room1.html";
 }
+
+function showPopup0() {
+    alert("לחצת על חלון 1!");
+}
+function showPopup1() {
+    alert("לחצת על חלון 2!");
+}
+function showPopup2() {
+    alert("לחצת על חלון 3!");
+}
