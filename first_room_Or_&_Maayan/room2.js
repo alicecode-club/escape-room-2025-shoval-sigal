90c13355102f711536d4afd12e6c007f6aa3ea23
maayank19 <mayaank1030@gmail.com>
2025-05-29 19:18:34 +0300

    editing room2

--- first_room_Or_&_Maayan/room2.js
+++ first_room_Or_&_Maayan/room2.js
@@ -1,3 +1,28 @@
 function hidePopup() {
     document.getElementById("popup").style.display = "none";
 }
+let num=0;
+
+function showPopup0() {
+    document.getElementById("popup").style.display = "block";  
+}
+
+function hidePopup() {
+    document.getElementById("popup").style.display = "none";
+}
+
+function showPopup1() {
+    document.getElementById("popup1").style.display = "block";
+}
+
+function hidePopup1() {
+    document.getElementById("popup1").style.display = "none";
+}
+
+function showPopup2() {
+    document.getElementById("popup2").style.display = "block";
+}
+
+function hidePopup2() {
+    document.getElementById("popup2").style.display = "none";
+}
