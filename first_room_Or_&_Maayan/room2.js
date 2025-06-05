adcf98ec78e390c39d71a237669cd6dd0ac055a8
TheGhoustking <orarzuan555@gmail.com>
2025-06-05 18:55:13 +0300

    still need their link

--- first_room_Or_&_Maayan/room2.js
+++ first_room_Or_&_Maayan/room2.js
@@ -9,31 +9,46 @@ function hidePopup() {
 function showPopup1() {
     document.getElementById("popup1").style.display = "block";
     item1 =true;   
+    console.log("1st");
 }
 
 function hidePopup1() {
     document.getElementById("popup1").style.display = "none";
+    checkFounding()
 }
 
 function showPopup2() {
     document.getElementById("popup2").style.display = "block";
     item2 = true;
+    console.log("2nd");
 }
 
 function hidePopup2() {
     document.getElementById("popup2").style.display = "none";
+    checkFounding()
 }
 
 function showPopup3() {
     document.getElementById("popup3").style.display = "block";
     item3 = true;
+    console.log("3rd");
 }
 
 function hidePopup3() {
     document.getElementById("popup3").style.display = "none";
+    checkFounding()
 }
 
-if (item1==true && item2==true && item3==true){
-    alert("Congrargulation! You Finished The First Room!!!")
-    //ניווט לחדר של נועה ואופיר
+/*function moveOn3() {
+    window.location.replace("/////");
+    console.log("hello");
+}*/
+
+function checkFounding(){
+    if (item1==true && item2==true && item3==true){
+        alert("Congratulations! You Finished The First Room!!!");
+        console.log("wow");
+        //moveOn3()
+        //ניווט לחדר של נועה ואופיר
+    }
 }
