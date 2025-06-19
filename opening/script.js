7cb0c1caaaad9344a8caaf294fd674d5c7d9e810
TheGhoustking <orarzuan555@gmail.com>
2025-06-19 19:14:13 +0300

    finishing opening

--- opening/script.js
+++ opening/script.js
@@ -3,6 +3,7 @@ const pages = [
   document.getElementById("p1"),
   document.getElementById("p2"),
   document.getElementById("p3"),
+  document.getElementById("p4"),
 ];
 
 function nextPage() {
@@ -17,4 +18,21 @@ function prevPage() {
     current--;
     pages[current].classList.remove("flipped");
   }
+}
+
+function nextPage() {
+  if (current < pages.length) {
+    pages[current].classList.add("flipped");
+    current++;
+
+    // ברגע שהגענו לעמוד האחרון
+    if (current === pages.length) {
+      setTimeout(() => moveOn(), 1000);
+    }
+  }
+}
+
+
+function moveOn() {
+      window.location.href = "../first room/code/room1.html";
 }
